3607db1f94686333a2f5b64cc5058052f2537875
fabricio-cesur <fabricio.s399523@cesurformacion.com>
2025-11-18 10:27:28 +0100

    FEAT: Encontrar el GameManager automáticamente

--- Assets/Scripts/MainButton.cs
+++ Assets/Scripts/MainButton.cs
@@ -7,6 +7,11 @@ public class MainButton : MonoBehaviour
     public GameManager gm;
     public TextMeshProUGUI muestraPuntos;
 
+    void Start()
+    {
+        gm = FindAnyObjectByType<GameManager>();
+    }
+
     public void sumarPuntos()
     {
         int puntos = gm.getPuntos();
